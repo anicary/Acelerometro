fc94715132601c7e4883d671df7e213466c9e463
name <carobox96@gmail.com>
2018-05-14 19:01:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/oliver/acelerometroxioami/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.oliver.acelerometroxioami;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private Sensor mysensor;
+    private SensorManager senman;
+    private TextView ejex, ejey, ejez;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        senman = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mysensor=senman.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        ejex = (TextView) findViewById(R.id.ejex);
+        ejey = (TextView) findViewById(R.id.ejey);
+        ejez = (TextView) findViewById(R.id.ejez);
+
+        senman.registerListener((SensorEventListener) this,mysensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        ejex.setText(""+sensorEvent.values[0]);
+        ejey.setText(""+sensorEvent.values[1]);
+        ejez.setText(""+sensorEvent.values[2]);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
